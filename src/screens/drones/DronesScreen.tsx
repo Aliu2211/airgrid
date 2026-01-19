188139d4837f4011a2ae7e4c0a3c9d8a7c9a7cf2
Aliu Tijani <aliutijani21@gmail.com>
2026-01-19 22:03:24 +0000

    refactor: restructure DronesScreen into folder with separate styles

---
+++ src/screens/drones/DronesScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './DronesScreen.styles';
+
+export const DronesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Drones Screen</Text>
+    </View>
+  );
+};
---
+++ src/screens/drones/index.ts
@@ -0,0 +1,1 @@
+export {DronesScreen} from './DronesScreen';
