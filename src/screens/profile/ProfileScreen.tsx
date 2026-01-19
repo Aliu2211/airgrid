b37ec5a64b3bdf2634169d2e1dcdb67ad2c3b810
Aliu Tijani <aliutijani21@gmail.com>
2026-01-19 22:04:49 +0000

    refactor: restructure ProfileScreen into folder with separate styles

---
+++ src/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './ProfileScreen.styles';
+
+export const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Profile Screen</Text>
+    </View>
+  );
+};
