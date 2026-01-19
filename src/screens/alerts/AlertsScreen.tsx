9baa20e3d1809d74710b41e9bea1fa08294e3adb
Aliu Tijani <aliutijani21@gmail.com>
2026-01-19 22:04:09 +0000

    refactor: restructure AlertsScreen into folder with separate styles

---
+++ src/screens/alerts/AlertsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './AlertsScreen.styles';
+
+export const AlertsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Alerts Screen</Text>
+    </View>
+  );
+};
